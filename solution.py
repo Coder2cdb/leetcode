95a32c90ec6a2ace365b7766dda8d6ca40f125bc
caiminchao <cmc930726@163.com>
2019-06-06 21:02:16 +0700

    leetcode 21

--- solution.py
+++ solution.py
@@ -411,6 +411,26 @@ class Solution(object):
         _dfs(n, n, '', res)
         return res
 
+    # leetcode21
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        递归算法
+        """
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+
 
 if __name__ == '__main__':
     print Solution().generateParenthesis(3)
